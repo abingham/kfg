34594eff109424da7299886cd6576802d6a82ff1
Austin Bingham <austin.bingham@gmail.com>
2018-06-29 12:23:01 +0200

    Fixed failing test.

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -4,7 +4,7 @@ import pytest
 def test_transform_violation_raises_ConfigValueError(config, key):
     config[key] = 'bar'
     config.set_transform(
-        path=key,
+        key=key,
         transform=int,
     )
     with pytest.raises(ConfigValueError):
